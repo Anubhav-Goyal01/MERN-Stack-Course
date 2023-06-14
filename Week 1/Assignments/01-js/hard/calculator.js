f32215b479029fe7863267b1c6332ee7248dbe6d
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-06-14 11:46:37 +0530

    Assignment 1 hard completed

--- Week 1/Assignments/01-js/hard/calculator.js
+++ Week 1/Assignments/01-js/hard/calculator.js
@@ -16,7 +16,144 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-calculator`
 */
+class Calculator {
+  constructor() {
+    this.result = 0
+  }
+  add(n) {
+    this.result = this.result + n
+  }
+  subtract(n) {
+    this.result = this.result - n
+  }
+  multiply(n) {
+    this.result = this.result * n
+  }
+  divide(n) {
+    if (n === 0){
+      throw new Error("error")
+    }
+    this.result = this.result / n
+  }
+  clear(n) {
+    this.result = 0
+  }
+  getResult(n) {
+    return this.result
+  }
 
-class Calculator {}
+  getPrecedence(operator) {
+    if (operator === "*" || operator === "/") {
+      return 2;
+    } else if (operator === "+" || operator === "-") {
+      return 1;
+    }
+    return 0;
+  }
+
+  containsinvalidChars(str){
+    const pattern = /[a-zA-Z]/
+    return pattern.test(str)
+  }
+
+  calculate(str) {
+    str = str.replace(/\s/g, "")
+    if (this.containsinvalidChars(str)){
+      throw new Error("Invalid characters")
+    }
+    const arr = str.match(/\d+(\.\d+)?|\+|\-|\*|\/|\(|\)/g, "")
+    const obj = {}
+    for (let token of arr){
+      if (token in obj){
+        obj[token] += 1
+      } else if ((token == ")") && !("(" in obj)){
+        throw new Error("error")
+      } else {
+        obj[token] = 1
+      }
+    }
+    if (obj["("] !== obj[")"]){
+      throw new Error("Error")
+    }
+
+    let stack = []
+    let operators = []
+    for (let token of arr) {
+      if (!isNaN(parseFloat(token))) {
+        stack.push(parseFloat(token))
+      } else if (token === "("){
+        operators.push(token)
+      } else if (token === ")"){
+        while (
+          operators.length > 0 && operators[operators.length - 1] !== "("
+        ){
+          const operator = operators.pop();
+          const num2 = stack.pop();
+          const num1 = stack.pop();
+
+          if (operator === "+") {
+            stack.push(num1 + num2);
+          } else if (operator === "-") {
+            stack.push(num1 - num2);
+          } else if (operator === "*") {
+            stack.push(num1 * num2);
+          } else if (operator === "/") {
+            if (num2 === 0){
+              throw new Error("error")
+            }
+            stack.push(num1 / num2);
+          }
+        }
+        operators.pop()
+      } else  {
+        while (
+          operators.length > 0 &&
+          this.getPrecedence(operators[operators.length - 1]) >= this.getPrecedence(token)
+        ) {
+
+          let operator = operators.pop()
+          const num2 = stack.pop()
+          const num1 = stack.pop()
+
+          if (operator === "+") {
+            stack.push(num1 + num2);
+          } else if (operator === "-") {
+            stack.push(num1 - num2);
+          } else if (operator === "*") {
+            stack.push(num1 * num2);
+          } else if (operator === "/") {
+            if (num2 === 0) {
+              throw new Error("error")
+            }
+            stack.push(num1 / num2);
+          }
+        }
+        operators.push(token)
+      }
+    }
+    
+    while (operators.length > 0) {
+      const operator = operators.pop();
+      const num2 = stack.pop();
+      const num1 = stack.pop();
+
+      if (operator === "+") {
+        stack.push(num1 + num2);
+      } else if (operator === "-") {
+        stack.push(num1 - num2);
+      } else if (operator === "*") {
+        stack.push(num1 * num2);
+      } else if (operator === "/") {
+        if (num2 === 0){
+          throw new Error("Divide by zero error")
+        }
+        stack.push(num1 / num2);
+      }
+    }
+    this.result = stack.pop()
+    return this.getResult()
+  }
+}
+
 
 module.exports = Calculator;
