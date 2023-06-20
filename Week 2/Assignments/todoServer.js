9ca2e0ef262df4a864657a237ea03d65a4ce22d1
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-06-20 19:18:50 +0530

    completed todos assignment

--- Week 2/Assignments/todoServer.js
+++ Week 2/Assignments/todoServer.js
@@ -42,6 +42,104 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require("fs");
+const { Server } = require('http');
 
-app.use(bodyParser.json());
+let todos
+
+fs.readFile("todos.json", {encoding: "utf-8"}, (err, data) => {
+  if (err) throw new Error(err)
+  todos = JSON.parse(data)
+})
+
+function writeTodos(todos){
+  const todosJSON = JSON.stringify(todos, null, 2)
+  fs.writeFile('todos.json', todosJSON, (err) => {
+      if (err) throw new Error(err)
+      console.log('Todos successfully written to file.');
+  });
+}
+
+app.use(bodyParser.json())
+
+let id = 0
+
+function createTODO(title, completed, description){
+  let todo = {
+    id: id,
+    title: title,
+    completed: completed,
+    description: description
+  }
+  todos.push(todo)
+  writeTodos(todos)
+}
+
+app.post('/todos', (req, res) => {
+  let {title, completed, description} = req.body
+  id += 1
+  createTODO(title, completed, description)
+  res.status(201).send({id: id})
+})
+
+app.get('/todos', (req, res) => {
+  res.json(todos)
+})
+
+app.get(`/todos/:id`, (req, res) => {
+  const todoId = req.params.id
+  if (!isNaN(todoId)){
+    const todo = todos.find((todo) => todo.id === parseInt(todoId))
+    if (todo){
+      res.json(todo)
+    } else{
+      res.status(494).send("404 Not Found")
+    }
+  } else{
+    res.status(404).send("404 Not Found")
+  }
+})
+
+
+app.put('/todos/:id', (req, res) => {
+  const {title, completed} = req.body
+  const todoId = req.params.id
+  if (!isNaN(todoId)){
+    const foundTodo = todos.find((todo) => todo.id === parseInt(todoId));
+    if (foundTodo) {
+      foundTodo.title = title;
+      foundTodo.completed = completed;
+      writeTodos(todos)
+      res.send();
+    } else {
+      res.status(404).send("404 Not Found");
+    }
+  } else{
+    res.status(404).send("404 Not Found")
+  }
+})  
+
+
+app.delete('/todos/:id', (req, res) => {
+  const todoId = req.params.id
+  if (!isNaN(todoId)){
+    const foundTodoIndex = todos.findIndex((todo) => todo.id === parseInt(todoId));
+    if (foundTodoIndex !== -1) {
+      todos.splice(foundTodoIndex, 1)
+      writeTodos(todos)
+      res.send();
+    } else {
+      res.status(404).send("404 Not Found");
+    }
+  } else{
+    res.status(404).send("404 Not Found")
+  }
+})  
+
+app.use((req, res) => {
+  res.status(404).send("404 Not Found")
+})
+
+app.listen(3000)
+
 module.exports = app;
