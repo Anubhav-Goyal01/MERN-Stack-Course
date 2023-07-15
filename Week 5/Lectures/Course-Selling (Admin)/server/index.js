f0d4a3313371c836af0c1bfff2703674516a2ddf
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-07-15 10:50:38 +0530

    Fixed login bug

--- Week 5/Lectures/Course-Selling (Admin)/server/index.js
+++ Week 5/Lectures/Course-Selling (Admin)/server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
@@ -9,12 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect(
+  "mongodb+srv://anubhav:anubhav@cluster0.eldzzyy.mongodb.net/",
+  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" }
+);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
