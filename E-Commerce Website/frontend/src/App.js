f009424ad72165df5b8441584375a3e8a3f9500e
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-08-07 22:31:14 +0530

    Home page template completed

---
+++ E-Commerce Website/frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
