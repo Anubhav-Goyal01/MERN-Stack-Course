05ac66dba373c890b8be7f3492ea228a8c7e29af
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-08-15 11:30:16 +0530

    deployment related action

--- Week 9/part-2-scripts/deploy.sh
+++ Week 9/part-2-scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-cd Week-5-with-axios-and-backend
- git pull origin master
+cd Week-9
  cd server
  pm2 kill
  pm2 start index.js
